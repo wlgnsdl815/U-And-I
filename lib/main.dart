14f41e10c49a85125dbf92d45d5db3da65df0123
wlgnsdl815 <wlgnsdl815@gmail.com>
2022-12-28 21:45:33 +0900

    U And I

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:u_and_i/Screens/home_screen.dart';
+
+void main() => runApp(MaterialApp(
+    theme: ThemeData(
+      fontFamily: 'sunflower',
+      textTheme: TextTheme(
+        headline1: TextStyle(
+          color: Colors.white,
+          fontFamily: 'parisienne',
+          fontSize: 80.0,
+        ),
+        headline2: TextStyle(
+          color: Colors.white,
+          fontSize: 50.0,
+          fontWeight: FontWeight.w700,
+        ),
+        bodyText1: TextStyle(
+          color: Colors.white,
+          fontSize: 30.0,
+        ),
+        bodyText2: TextStyle(
+          color: Colors.white,
+          fontSize: 20.0,
+        ),
+      ),
+    ),
+    debugShowCheckedModeBanner: false,
+    home: HomeScreen()));
